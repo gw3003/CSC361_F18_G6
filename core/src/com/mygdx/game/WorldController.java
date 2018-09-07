5af9727bbe6cc0770e63c7eaa2c4a4436fe8abc8
Kevin Rutter <kr7700@cs.ship.edu>
2018-09-07 16:30:26 -0400

    Added WorldController (for real this time)

---
+++ core/src/com/mygdx/game/WorldController.java
@@ -0,0 +1,38 @@
+package com.mygdx.game;
+
+/**
+ * @author Kevin Rutter
+ * Contains controls for the game, such as for the camera, movement, etc.
+ */
+public class WorldController
+{
+	// Tag used for logging purposes
+	private static final String TAG =
+			WorldController.class.getName();
+	
+	/**
+	 * Constructor for WorldController.
+	 */
+	public WorldController()
+	{
+		
+	}
+	
+	/**
+	 * Initialization code for WorldController.
+	 * Useful to call when resetting objects.
+	 */
+	public void init()
+	{
+		
+	}
+	
+	/**
+	 * Applies updates to the game world many times a second.
+	 * @param deltaTime		How much time has passed since last frame.
+	 */
+	public void update(float deltaTime)
+	{
+		
+	}
+}
