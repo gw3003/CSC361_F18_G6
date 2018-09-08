5c9f9063ada577f925b569f0c9ecf34cd6386524
Kevin Rutter <kr7700@cs.ship.edu>
2018-09-08 18:50:54 -0400

    Regenerated libgdx directory, fixed some typos

---
+++ core/src/com/packetpub/libgdx/canyonbunny/util/Constants.java
@@ -0,0 +1,14 @@
+package com.packetpub.libgdx.canyonbunny.util;
+
+/**
+ * @author Gabe Werick
+ * This class contains the Constants to be used in the game
+ */
+public class Constants
+{
+	// Visible game world is 5 meters wide
+	public static final float VIEWPORT_WIDTH = 5.0f;
+
+	// Visible game world is 5 meters tall
+	public static final float VIEWPORT_HEIGHT = 5.0f;
+}
---
+++ desktop/src/com/packetpub/libgdx/canyonbunny/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.packetpub.libgdx.canyonbunny.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.packetpub.libgdx.canyonbunny.CanyonBunnyMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CanyonBunnyMain(), config);
+	}
+}
