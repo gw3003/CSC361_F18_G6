71c5514f520e0be25b900e8f5a311652386187fc
Unknown <tf0199@cs.ship.edu>
2018-09-11 16:10:48 -0400

    Added lines to Desktop launcher and Assets-raw/images folders

--- desktop/src/com/packetpub/libgdx/canyonbunny/desktop/DesktopLauncher.java
+++ desktop/src/com/packetpub/libgdx/canyonbunny/desktop/DesktopLauncher.java
@@ -3,9 +3,21 @@ package com.packetpub.libgdx.canyonbunny.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.packetpub.libgdx.canyonbunny.CanyonBunnyMain;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 
 public class DesktopLauncher {
+	private static boolean rebuildAtlas = true;
+	private static boolean drawDebugOutline = true;
 	public static void main (String[] arg) {
+		if (rebuildAtlas) {
+			Settings settings = new Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.duplicatePadding = false;
+			settings.debug = drawDebugOutline;
+			TexturePacker.process(settings, "assets-raw/images", "../CanyonBunny-android/assets/images","canyonbunny.pack");
+		}
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new CanyonBunnyMain(), config);
 	}
