eb08c8a0aae14d043da60c88bab0ebcc90c23bc9
Gabe <gw3003@cs.ship.edu>
2018-09-12 13:26:59 -0400

    Added Several Asset classes
    Added Rock, Bunny head, Feather, gold coin classes

--- core/src/com/packetpub/libgdx/canyonbunny/game/Assets.java
+++ core/src/com/packetpub/libgdx/canyonbunny/game/Assets.java
@@ -13,34 +13,32 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 
 /**
- * @author Kevin Rutter
- * Handles texture loading using a texture atlas.
+ * @author Kevin Rutter Handles texture loading using a texture atlas.
  */
-public class Assets implements Disposable, AssetErrorListener
-{
+public class Assets implements Disposable, AssetErrorListener {
 	public static final String TAG = Assets.class.getName();
-	
+
 	public static final Assets instance = new Assets();
-	
+
 	private AssetManager assetManager;
-	
+
 	public AssetBunny bunny;
 	public AssetRock rock;
 	public AssetGoldCoin goldCoin;
 	public AssetFeather feather;
 	public AssetLevelDecoration levelDecoration;
-	
+
 	// singleton: prevent instantiation from other classes
-	private Assets()
-	{
+	private Assets() {
 	}
-	
+
 	/**
 	 * Load up the texture atlas.
-	 * @param assetManager	The asset manager this class will use.
+	 * 
+	 * @param assetManager
+	 *            The asset manager this class will use.
 	 */
-	public void init(AssetManager assetManager)
-	{
+	public void init(AssetManager assetManager) {
 		this.assetManager = assetManager;
 		// set asset manager error handler
 		assetManager.setErrorListener(this);
@@ -51,16 +49,14 @@ public class Assets implements Disposable, AssetErrorListener
 		Gdx.app.debug(TAG, "# of assets loaded: " + assetManager.getAssetNames().size);
 		for (String a : assetManager.getAssetNames())
 			Gdx.app.debug(TAG, "asset: " + a);
-		
-		
+
 		TextureAtlas atlas = assetManager.get(Constants.TEXTURE_ATLAS_OBJECTS);
-		
+
 		// enable texture filtering for pixel smoothing
-		for (Texture t : atlas.getTextures())
-		{
-			t.setFilter(TextureFilter.Linear,  TextureFilter.Linear);
+		for (Texture t : atlas.getTextures()) {
+			t.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		}
-		
+
 		// create game resource objects
 		bunny = new AssetBunny(atlas);
 		rock = new AssetRock(atlas);
@@ -68,45 +64,42 @@ public class Assets implements Disposable, AssetErrorListener
 		feather = new AssetFeather(atlas);
 		levelDecoration = new AssetLevelDecoration(atlas);
 	}
-	
-	
+
 	/**
 	 * Tell the asset manager to to unload assets.
 	 */
 	@Override
-	public void dispose()
-	{
+	public void dispose() {
 		assetManager.dispose();
 	}
-	
+
 	/**
-	 * Called when the asset manager has an error with an asset, prints out an error log.
+	 * Called when the asset manager has an error with an asset, prints out an error
+	 * log.
 	 */
 	@Override
-	public void error(AssetDescriptor asset, Throwable throwable)
-	{
-		Gdx.app.error(TAG, "Couldn't load asset '" + asset.fileName + "'", (Exception)throwable);		
+	public void error(AssetDescriptor asset, Throwable throwable) {
+		Gdx.app.error(TAG, "Couldn't load asset '" + asset.fileName + "'", (Exception) throwable);
 	}
-	
+
 	/**
-	 * @author Kevin Rutter
-	 * Initializes and holds the game's decorative textures.
+	 * @author Kevin Rutter Initializes and holds the game's decorative textures.
 	 */
-	public class AssetLevelDecoration
-	{
+	public class AssetLevelDecoration {
 		public final AtlasRegion cloud01;
 		public final AtlasRegion cloud02;
 		public final AtlasRegion cloud03;
 		public final AtlasRegion mountainLeft;
 		public final AtlasRegion mountainRight;
 		public final AtlasRegion waterOverlay;
-		
+
 		/**
 		 * Initialize atlas regions.
-		 * @param atlas		The texture atlas being used.
+		 * 
+		 * @param atlas
+		 *            The texture atlas being used.
 		 */
-		public AssetLevelDecoration(TextureAtlas atlas)
-		{
+		public AssetLevelDecoration(TextureAtlas atlas) {
 			cloud01 = atlas.findRegion("cloud01");
 			cloud02 = atlas.findRegion("cloud02");
 			cloud03 = atlas.findRegion("cloud03");
@@ -115,5 +108,75 @@ public class Assets implements Disposable, AssetErrorListener
 			waterOverlay = atlas.findRegion("water_overlay");
 		}
 	}
-	
+
+	/**
+	 * @author Gabe Werick This class holds info for the Bunny Head texture
+	 */
+	public class AssetBunny {
+		public final AtlasRegion head;
+
+		/**
+		 * Sets head to hold the reference to the correct region for bunny_head
+		 * 
+		 * @param atlas
+		 *            Texture atlas
+		 */
+		public AssetBunny(TextureAtlas atlas) {
+			head = atlas.findRegion("bunny_head");
+		}
+	}
+
+	/**
+	 * @author Gabe Werick This class holds info for rock edge and middle texture
+	 */
+	public class AssetRock {
+		public final AtlasRegion edge;
+		public final AtlasRegion middle;
+
+		/**
+		 * sets edge and middle to hold references to the appropriate areas
+		 * 
+		 * @param atlas
+		 *            Texture atlas
+		 */
+		public AssetRock(TextureAtlas atlas) {
+			edge = atlas.findRegion("rock_edge");
+			middle = atlas.findRegion("rock_middle");
+		}
+	}
+
+	/**
+	 * @author Gabe Werick This class holds info for the Gold Coin texture
+	 */
+	public class AssetGoldCoin {
+		public final AtlasRegion goldCoin;
+
+		/**
+		 * Sets head to hold the reference to the correct region for Gold Coin
+		 * 
+		 * @param atlas
+		 *            Texture atlas
+		 */
+		public AssetGoldCoin(TextureAtlas atlas) {
+			goldCoin = atlas.findRegion("item_gold_coin");
+		}
+	}
+
+	/**
+	 * @author Gabe Werick This class holds info for the Feather texture
+	 */
+	public class AssetFeather {
+		public final AtlasRegion feather;
+
+		/**
+		 * Sets head to hold the reference to the correct region for Gold Coin
+		 * 
+		 * @param atlas
+		 *            Texture atlas
+		 */
+		public AssetFeather(TextureAtlas atlas) {
+			feather = atlas.findRegion("item_feather");
+		}
+	}
+
 }
