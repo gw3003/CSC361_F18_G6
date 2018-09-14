662c498ab113733c4982df28f4166c5e45ff62ad
Gabe <gw3003@cs.ship.edu>
2018-09-14 13:52:14 -0400

    Started to make Level
    wrote import statements

---
+++ core/src/com/packetpub/libgdx/canyonbunny/game/Level.java
@@ -0,0 +1,15 @@
+package com.packetpub.libgdx.canyonbunny.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.libgdx.canyonbunn.game.objects.AbstractGameObject;
+import com.packetpub.libgdx.canyonbunny.game.objects.Clouds;
+import com.packetpub.libgdx.canyonbunny.game.objects.Mountains;
+import com.packetpub.libgdx.canyonbunny.game.objects.Rock;
+import com.packetpub.libgdx.canyonbunny.game.objects.Wateroverlay;
+
+public class Level {
+	
+}
