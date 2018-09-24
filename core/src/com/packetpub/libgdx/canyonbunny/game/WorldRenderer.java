5562c3ad2286c61388a7c9d6e63cde7da3d78296
Gabe <gw3003@cs.ship.edu>
2018-09-23 23:37:23 -0400

    Added methods to display powerups and game over
    The game can now display information related to the feather powerup and a game over screen

--- core/src/com/packetpub/libgdx/canyonbunny/game/WorldRenderer.java
+++ core/src/com/packetpub/libgdx/canyonbunny/game/WorldRenderer.java
@@ -159,11 +159,61 @@ public class WorldRenderer implements Disposable
 		//draw collected gold coins icokn + text
 		// (anchored to top left edge)
 		renderGuiScore(batch);
+		//draw collected feather icon (anchored to top left edge)
+		renderGuiFeatherPowerup(batch);
 		//draw extra lives incom + text (anchored to top right edge)
 		renderGuiExtraLive(batch);
 		//draw FPS text (anchored to bottom right edge)
 		renderGuiFpsCounter(batch);
+		//draw game over text
+		renderGuiGameOverMessage(batch);
 		batch.end();
+	}
+	
+	/**
+	 * Renders a game over message on the screen when called
+	 * @param batch the Spritebatch object
+	 */
+	private void renderGuiGameOverMessage(SpriteBatch batch)
+	{
+		float x = cameraGUI.viewportWidth / 2;
+		float y = cameraGUI.viewportHeight / 2;
+		if(worldController.isGameOver())
+		{
+			BitmapFont fontGameOver = Assets.instance.fonts.defaultBig;
+			fontGameOver.setColor(1, 0.75f, 0.25f, 1);
+			fontGameOver.drawMultiLine(batch, "GAME OVER", x , y, 0, BitmapFont.HAlignment.CENTER);
+			fontGameOver.setColor(1, 1, 1, 1);
+		}
+	}
+	
+	/**
+	 * If the feather powerup is active this handle showing the powerup
+	 * @param batch the spritebatch object
+	 */
+	private void renderGuiFeatherPowerup(SpriteBatch batch)
+	{
+		float x = -15;
+		float y = 30;
+		float timeLeftFeatherPowerup = worldController.level.bunnyHead.timeLeftFeatherPowerup;
+		
+		if(timeLeftFeatherPowerup > 0)
+		{
+			/*start icon fade in/out if the left power-up time
+			is less than 4 seconds. The fade interval is set to 5 changes per second
+			*/
+			if(timeLeftFeatherPowerup < 4)
+			{
+				if(((int)(timeLeftFeatherPowerup * 5) % 2) != 0)
+				{
+					batch.setColor(1, 1, 1, 0.5f);
+				}
+			}
+			
+			batch.draw(Assets.instance.feather.feather, x, y, 50, 50, 100, 100, 0.35f, -0.35f, 0);
+			batch.setColor(1, 1, 1, 1);
+			Assets.instance.fonts.defaultSmall.draw(batch, "" + (int)timeLeftFeatherPowerup, x + 60, y + 57);
+		}
 	}
 
 	/**
