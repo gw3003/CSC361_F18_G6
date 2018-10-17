6c3a874575486d7d276eb40885353214cdaa6283
Unknown <tf0199@cs.ship.edu>
2018-10-16 20:31:46 -0400

    Abstract class update

--- core/src/com/packetpub/libgdx/canyonbunny/game/objects/AbstractGameObject.java
+++ core/src/com/packetpub/libgdx/canyonbunny/game/objects/AbstractGameObject.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.physics.box2d.Body;
 
 /**
  * Makes an abstract class for all game objects to structure themselves around
@@ -25,6 +26,7 @@ public abstract class AbstractGameObject
 
 	public Vector2 acceleration;
 	public Rectangle bounds;
+	public Body body;
 
 	/**
 	 * Builds the game object
@@ -96,11 +98,19 @@ public abstract class AbstractGameObject
 	 */
 	public void update(float deltaTime)
 	{
+		if (body == null)
+		{
 		updateMotionX(deltaTime);
 		updateMotionY(deltaTime);
 		// Move to new position
 		position.x += velocity.x * deltaTime;
 		position.y += velocity.y * deltaTime;
+		}
+		else
+		{
+			position.set(body.getPosition());
+			rotation = body.getAngle() * MathUtils.radiansToDegrees;
+		}
 	}
 
 	/**
